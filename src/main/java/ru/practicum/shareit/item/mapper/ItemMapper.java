7e15273dae1d93e50e7df8f7a2d7bf9c54da3d83
Tumar Amirzhan <amiramutt@gmail.com>
2025-08-08 01:11:53 +0500

    Sprint 14

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -13,6 +13,7 @@ public class ItemMapper {
         if (item == null) {
             return null;
         }
+        
         return CreateItemDto.builder()
                 .id(item.getId())
                 .name(item.getName())
@@ -26,6 +27,7 @@ public class ItemMapper {
         if (createItemDto == null) {
             return null;
         }
+
         return Item.builder()
                 .name(createItemDto.getName())
                 .description(createItemDto.getDescription())
